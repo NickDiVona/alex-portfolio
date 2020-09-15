6c931b5826b89f5c4ca3be6f112817fce20d9188
Nick DiVona <skatedivona@gmail.com>
2020-09-14 19:58:48 -0700

    setup device width in redux

--- src/redux/actionCreators.tsx
+++ src/redux/actionCreators.tsx
@@ -6,3 +6,33 @@ export const changeScreen = (screen: string) => {
     payload: screen
   };
 };
+
+export const changeToSmallPhone = () => {
+  return {
+    type: types.CHANGE_DEVICE_TO_SMALL_PHONE
+  };
+};
+
+export const changeToMediumPhone = () => {
+  return {
+    type: types.CHANGE_DEVICE_TO_MEDIUM_PHONE
+  };
+};
+
+export const changeToLargePhone = () => {
+  return {
+    type: types.CHANGE_DEVICE_TO_LARGE_PHONE
+  };
+};
+
+export const changeToTablet = () => {
+  return {
+    type: types.CHANGE_DEVICE_TO_TABLET
+  };
+};
+
+export const changeToDesktop = () => {
+  return {
+    type: types.CHANGE_DEVICE_TO_DESKTOP
+  };
+};
--- src/redux/reducers/currentDeviceTypeReducer.tsx
+++ src/redux/reducers/currentDeviceTypeReducer.tsx
@@ -20,12 +20,60 @@ const INITIAL_STATE = {
   grayContainerWidth: '80%',
   titleTextSize: 24,
   yellowTitleTextSize: 14,
-  descriptionTextSize: 12,
-  minimumScreenSize: 769
+  descriptionTextSize: 12
 };
 
 const currentDeviceTypeReducer = (state = INITIAL_STATE, action: Action) => {
   switch (action.type) {
+    // 360 or narrower
+    case types.CHANGE_DEVICE_TO_SMALL_PHONE:
+      return {
+        device: 'smallPhone',
+        grayContainerWidth: '80%',
+        titleTextSize: 24,
+        yellowTitleTextSize: 14,
+        descriptionTextSize: 12
+      };
+
+    // 375 or narrower
+    case types.CHANGE_DEVICE_TO_MEDIUM_PHONE:
+      return {
+        device: 'mediumPhone',
+        grayContainerWidth: '80%',
+        titleTextSize: 24,
+        yellowTitleTextSize: 14,
+        descriptionTextSize: 12
+      };
+
+    // 414 or narrower
+    case types.CHANGE_DEVICE_TO_LARGE_PHONE:
+      return {
+        device: 'largePhone',
+        grayContainerWidth: '80%',
+        titleTextSize: 24,
+        yellowTitleTextSize: 14,
+        descriptionTextSize: 12
+      };
+
+    // 768 or narrower
+    case types.CHANGE_DEVICE_TO_TABLET:
+      return {
+        device: 'tablet',
+        grayContainerWidth: '80%',
+        titleTextSize: 24,
+        yellowTitleTextSize: 14,
+        descriptionTextSize: 12
+      };
+
+    case types.CHANGE_DEVICE_TO_DESKTOP:
+      return {
+        device: 'tablet',
+        grayContainerWidth: '80%',
+        titleTextSize: 24,
+        yellowTitleTextSize: 14,
+        descriptionTextSize: 12
+      };
+
     default:
       return state;
   }
