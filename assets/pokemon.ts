26899ecdff0e38c5139ee424e4c31e969eb41643
Nick DiVona <skatedivona@gmail.com>
2020-10-30 17:01:24 -0700

    rendering list of pokemon

--- assets/pokemon.ts
+++ assets/pokemon.ts
@@ -1,16 +1,16 @@
 export const pokemonList = [
-  { name: 'cabeknife', image: require('./images/Pokemon/cabeknife.png') },
+  { name: 'cubeknife', image: require('./images/Pokemon/cubeknife.png') },
   { name: 'evol', image: require('./images/Pokemon/evol.png') },
   { name: 'flareonwave2', image: require('./images/Pokemon/flareonwave2.png') },
   { name: 'golemanger', image: require('./images/Pokemon/golemanger.png') },
-  { name: 'lapraspat', image: require('./images/Pokemon/lapraspat.png') },
+  { name: 'lapraspat', image: require('./images/Pokemon/lapraspat.jpeg') },
   { name: 'lurk', image: require('./images/Pokemon/lurk.png') },
   { name: 'mewtwop2', image: require('./images/Pokemon/mewtwop2.png') },
   { name: 'polipalm', image: require('./images/Pokemon/polipalm.png') },
   { name: 'sip2', image: require('./images/Pokemon/sip2.png') },
   { name: 'vulpixlove2', image: require('./images/Pokemon/vulpixlove2.png') },
   { name: 'wabbasherers', image: require('./images/Pokemon/wabbasherers.png') },
-  { name: 'weavesmurk', image: require('./images/Pokemon/weavesmurk.png') },
+  { name: 'weavsmurk', image: require('./images/Pokemon/weavsmurk.png') },
   {
     name: 'wooperquestion',
     image: require('./images/Pokemon/wooperquestion.png')
--- src/components/contentDetailComponent.tsx
+++ src/components/contentDetailComponent.tsx
@@ -1,11 +1,43 @@
 //sys
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList } from 'react-native';
 import { useDimensions } from '@react-native-community/hooks';
 import { useSelector, useDispatch } from 'react-redux';
 //usr
+import { pokemonList } from '../../assets/pokemon';
+
+// interface CurrentDeviceTypeProps {
+//   [state: string]: {
+//     device: string;
+//     iconDimensions: number;
+//     iconUpperMargin: number;
+//     grayContainerWidth: string;
+//     titleTextSize: number;
+//     yellowTitleTextSize: number;
+//     descriptionTextSize: number;
+//   };
+// }
 
 const ContentDetails = () => {
+  const currentScreen = useSelector((state: any) => state.currentScreen);
+  const renderPokmonItem = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
+  if (currentScreen === 'pokemon') {
+    return (
+      <FlatList
+        data={pokemonList}
+        renderItem={renderPokmonItem}
+        keyExtractor={(item) => item.name.toString()}
+      />
+    );
+  }
+
   return (
     <View>
       <Text style={{ color: 'white', marginTop: 20 }}>Content</Text>
