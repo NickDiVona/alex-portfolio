03dda6f1cd92285822decf670bf612e56a109547
Nick DiVona <skatedivona@gmail.com>
2020-11-10 10:10:19 -0800

    Updates

---
+++ precache-manifest.602b78f9c77be3888c9b271a43e48c2d.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f919f3d33de25d444d2",
+    "url": "/static/js/app.a70b38a7.chunk.js"
+  },
+  {
+    "revision": "77cafc906f42b4a876d5",
+    "url": "/static/js/runtime~app.0dd01ab9.js"
+  },
+  {
+    "revision": "9d6f65b0f73e73eeae66",
+    "url": "/static/js/2.f6288fc2.chunk.js"
+  },
+  {
+    "revision": "5969833f13f97f783550c18d4cf958b8",
+    "url": "/static/media/alexChibi.5969833f.png"
+  },
+  {
+    "revision": "67dff11567106cf1d276b4e894eca3b5",
+    "url": "/static/media/cubeknife.67dff115.png"
+  },
+  {
+    "revision": "059905d87f1d36b09c12f2734dce2002",
+    "url": "/static/media/evol.059905d8.png"
+  },
+  {
+    "revision": "1574c06e091ac85e9b29d7a3700b918b",
+    "url": "/static/media/flareonwave2.1574c06e.png"
+  },
+  {
+    "revision": "62d60454ec57cd5dddb16060c0484a83",
+    "url": "/static/media/golemanger.62d60454.png"
+  },
+  {
+    "revision": "17d2cd21fef41ccd6b6a08c6f47a6052",
+    "url": "/static/media/lapraspat.17d2cd21.jpeg"
+  },
+  {
+    "revision": "0c24675ddb57a4dd232bc4f02d740f49",
+    "url": "/static/media/lurk.0c24675d.png"
+  },
+  {
+    "revision": "5fe7e6654ed13024490aa7399a16ea20",
+    "url": "/static/media/mewtwop2.5fe7e665.png"
+  },
+  {
+    "revision": "7c8afa41c43111407b7d8c63bb6715de",
+    "url": "/static/media/polipalm.7c8afa41.png"
+  },
+  {
+    "revision": "3523fedaad44697ebb0809aa9d7a4f6e",
+    "url": "/static/media/sip2.3523feda.png"
+  },
+  {
+    "revision": "6fc0ffdf773e4d2a6e83165e5f6f4065",
+    "url": "/static/media/vulpixlove2.6fc0ffdf.png"
+  },
+  {
+    "revision": "09167669bbb2005f4fb8af3360b21638",
+    "url": "/static/media/wabbasherers.09167669.png"
+  },
+  {
+    "revision": "b59d8817af98cbb76c4b5f8ea859b5d7",
+    "url": "/static/media/weavsmurk.b59d8817.png"
+  },
+  {
+    "revision": "b206cf5f5cf69d97331fdc29a255faa8",
+    "url": "/static/media/wooperquestion.b206cf5f.png"
+  },
+  {
+    "revision": "9f7e1199d8d7d2c68b7f3cec31033f28",
+    "url": "/static/media/databrain.9f7e1199.png"
+  },
+  {
+    "revision": "3764d0414a119982a7257f262beade4e",
+    "url": "/static/media/datacry.3764d041.png"
+  },
+  {
+    "revision": "ed849a372b8cd6116efc90aaa9127f2c",
+    "url": "/static/media/dataggBL.ed849a37.png"
+  },
+  {
+    "revision": "ca585e50721cf58b231a35fb26761eb8",
+    "url": "/static/media/gg.ca585e50.png"
+  },
+  {
+    "revision": "4277c971f297455a2cc5f44b285176d8",
+    "url": "/static/media/heart.4277c971.png"
+  },
+  {
+    "revision": "36be610749a04999ad1829ceeeba445e",
+    "url": "/static/media/lol.36be6107.png"
+  },
+  {
+    "revision": "e6781969c9adb6c1811ed65fca23af28",
+    "url": "/static/media/rip.e6781969.png"
+  },
+  {
+    "revision": "1a0b1ebf29091b7e0371e5ad21687075",
+    "url": "/static/media/wavefixed.1a0b1ebf.png"
+  },
+  {
+    "revision": "cc766e95bf864aa81b1a8f71f3864237",
+    "url": "/static/media/wtf.cc766e95.png"
+  },
+  {
+    "revision": "8915924a1102f182e62724936439282c",
+    "url": "/static/media/cry.8915924a.png"
+  },
+  {
+    "revision": "a1bf3662590b06933f35196bd7a0e3a0",
+    "url": "/static/media/dab.a1bf3662.png"
+  },
+  {
+    "revision": "ce523c19defe07a471d3c3d8079c7515",
+    "url": "/static/media/ghost.ce523c19.png"
+  },
+  {
+    "revision": "ffb008e69c76b3733308fbb3f2206fe7",
+    "url": "/static/media/heart.ffb008e6.png"
+  },
+  {
+    "revision": "f9229d91c011827bc7255f7e1de494ef",
+    "url": "/static/media/lurking.f9229d91.png"
+  },
+  {
+    "revision": "b953cc813dc11c7b25a279523829cbe4",
+    "url": "/static/media/mean3.b953cc81.png"
+  },
+  {
+    "revision": "985402f7eefb6210700b86d0bb02e535",
+    "url": "/static/media/omenrip.985402f7.png"
+  },
+  {
+    "revision": "9676a7c4ed36bc7ac5efa6374d5d6063",
+    "url": "/static/media/pommydragon2.9676a7c4.png"
+  },
+  {
+    "revision": "dbbda61a23c23f7ac54fb20652d7f2b3",
+    "url": "/static/media/vipertrash.dbbda61a.png"
+  },
+  {
+    "revision": "41241d498221abc9d5c1d1d5c150b748",
+    "url": "/static/media/wave.41241d49.png"
+  },
+  {
+    "revision": "71fee8bb3423d1214a422c333596ff83",
+    "url": "/static/media/beebadge2.71fee8bb.png"
+  },
+  {
+    "revision": "1818a5078efe35d3e1f49d572c849123",
+    "url": "/static/media/beebadge3.1818a507.png"
+  },
+  {
+    "revision": "dec170ebd568163d24bb2ca5bf962da6",
+    "url": "/static/media/beebadge4.dec170eb.png"
+  },
+  {
+    "revision": "691b7f66f8200f5bff0e74c7e8f652f9",
+    "url": "/static/media/egg.691b7f66.png"
+  },
+  {
+    "revision": "8a3253915e1f97c87a016847ee4ed106",
+    "url": "/static/media/knifebadge.8a325391.png"
+  },
+  {
+    "revision": "b60fc6cd0150246b11ed61d084851a7e",
+    "url": "/static/media/lapras1.b60fc6cd.png"
+  },
+  {
+    "revision": "6787b9abbc39f8f67faecf8096ecdd7f",
+    "url": "/static/media/lapras2.6787b9ab.png"
+  },
+  {
+    "revision": "b5a3864f8b56f6a458dd72b26dc24640",
+    "url": "/static/media/lapras3.b5a3864f.png"
+  },
+  {
+    "revision": "7940b6c801f4f5d7cf4d9ad99ae4f7bb",
+    "url": "/static/media/larvbadge.7940b6c8.png"
+  },
+  {
+    "revision": "e1c62ad5d52d518ed078114afea23b82",
+    "url": "/static/media/sagebadge.e1c62ad5.png"
+  },
+  {
+    "revision": "825540e5ae3cc56cef85e875dba58345",
+    "url": "/static/media/windmills.825540e5.jpg"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "f0a41fce6a41913d0750da190a5be32d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "f1ffc306e6990be1ae5b8474f36b32d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b16137dfed530129dba4d71c15615992",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
